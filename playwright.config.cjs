6c033530108885f8cdd86ad1f2715e01f4431ba3
suh1z <jobo.holmberg@gmail.com>
2024-08-10 22:41:27 +0300

    try

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -69,7 +69,7 @@ module.exports = defineConfig({
   ],
 
   webServer: {
-    command: 'npm run server',
+    command: 'npm run start-prod',
     url: 'http://localhost:3001',
     reuseExistingServer: !process.env.CI,
   },
